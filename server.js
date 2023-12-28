6022849c652bd7cccb5bd3d52babdd9e006a27fd
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-12-28 16:59:56 +0100

    chatApp is working correctly / msgs flow freely

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ const io = socket(server);
 
 io.on('connection', socket => {
 	console.log('New client! Its id – ' + socket.id);
-	socket.on('message', () => {
+	socket.on('message', message => {
 		console.log("Oh, I've got something from " + socket.id);
+		messages.push(message);
+		socket.broadcast.emit('message', message);
 	});
 	socket.on('disconnect', () => {
 		console.log('Oh, socket ' + socket.id + ' has left');
