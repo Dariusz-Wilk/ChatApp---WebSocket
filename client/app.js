fac45cb9d292820af87a09519fdf6befa8afb424
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-12-28 14:50:09 +0100

    prepare starting files / added const's / create login()

---
+++ client/app.js
@@ -0,0 +1,23 @@
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection = document.querySelector('#messages-section');
+const messagesList = document.querySelector('#messages-list');
+const addMessageForm = document.querySelector('#add-messages-form');
+const userNameInput = document.querySelector('#username');
+const messageContentInput = document.querySelector('#message-content');
+
+let userName;
+
+const login = e => {
+	e.preventDefault();
+	if (userNameInput.value === '') {
+		alert('Please write your name first');
+	} else {
+		userName = userNameInput.value;
+		loginForm.classList.remove('show');
+		messagesSection.classList.add('show');
+	}
+};
+
+loginForm.addEventListener('submit', e => {
+	login(e);
+});
